2ed2a724b6b8193fa011365afd3be19046c235a5
Revan <abusovrvan34@gmail.com>
2026-03-06 14:48:24 +0400

    student and course was handled

--- src/main/java/com/example/demo/models/Course.java
+++ src/main/java/com/example/demo/models/Course.java
@@ -1,4 +1,59 @@
 package com.example.demo.models;
 
+import jakarta.annotation.Nullable;
+
 public class Course {
+private  String courseId;
+private  String name;
+private  double price;
+private double money_earned;
+@Nullable
+private  Teacher teacher;
+
+
+    public Course(String name, double price) {
+        this.price = price;
+        this.name=name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMoney_earned() {
+        return money_earned;
+    }
+
+    public void setMoney_earned(double money_earned) {
+        this.money_earned = money_earned;
+    }
 }
--- src/main/java/com/example/demo/models/Student.java
+++ src/main/java/com/example/demo/models/Student.java
@@ -1,4 +1,59 @@
 package com.example.demo.models;
 
+import jakarta.annotation.Nullable;
+
 public class Student {
+    private String studentId;
+    private String name;
+    private String address;
+    private String email;
+    @Nullable
+    private Course course;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(@Nullable Course course) {
+        this.course = course;
+    }
+
+    public Student(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
 }
